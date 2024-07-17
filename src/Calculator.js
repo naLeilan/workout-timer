806b9f49e2bf2fa90f5d127a10a951f5d4d5eef8
naleilan <7776224+naLeilan@users.noreply.github.com>
2024-07-17 10:35:09 +0300

    added useEffect for playsound

--- src/Calculator.js
+++ src/Calculator.js
@@ -8,21 +8,33 @@ function Calculator({ workouts, allowSound }) {
   const [durationBreak, setDurationBreak] = useState(5);
   const [duration, setDuration] = useState(0);
 
-  const playSound = useCallback(
-    function () {
-      if (!allowSound) return;
-      const sound = new Audio(clickSound);
-      sound.play();
-    },
-    [allowSound]
-  );
+  // const playSound = useCallback(
+  //   function () {
+  //     if (!allowSound) return;
+  //     const sound = new Audio(clickSound);
+  //     sound.play();
+  //   },
+  //   [allowSound]
+  // );
 
   useEffect(
     function () {
       setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
+    },
+    [number, sets, speed, setDurationBreak]
+  );
+
+  useEffect(
+    function () {
+      const playSound = function () {
+        if (!allowSound) return;
+        const sound = new Audio(clickSound);
+        sound.play();
+      };
+
       playSound();
     },
-    [number, sets, speed, setDurationBreak, playSound]
+    [duration, allowSound]
   );
 
   // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
@@ -31,12 +43,10 @@ function Calculator({ workouts, allowSound }) {
 
   function handleInc() {
     setDuration((duration) => Math.floor(duration) + 1);
-    playSound();
   }
 
   function handleDec() {
     setDuration((duration) => (duration > 1 ? Math.ceil(duration) - 1 : 0));
-    playSound();
   }
 
   return (
